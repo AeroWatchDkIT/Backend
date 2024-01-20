3f5b73df2d487e6343c26a473a79d8f8597fbd66
KacperWro <adamczyk911119@gmail.com>
2024-01-20 15:59:21 +0000

    Began work on user endpoints

---
+++ Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PalletSyncApi.Services;
+using System.Text.Json;
+using PalletSyncApi.Classes;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PalletSyncApi.Controllers
+{
+    [ApiController]
+    [Route("Users")]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserService _userService;
+
+       
+
+    }
+}
---
+++ Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using PalletSyncApi.Classes;
+
+namespace PalletSyncApi.Services
+{
+    public interface IUserService
+    {
+        public Task<object> GetAllUsersAsync();
+        public Task AddUserAsync(Forklift forklift);
+        //public Task<object> SearchUsersAsync(SearchUser query);
+        public Task<object> GetUserByIdAsync(string id);
+        public Task<bool> DeleteUserByIdAsync(string id);
+        public Task<bool> UpdateUser(string newId, string oldId);
+    }
+}
