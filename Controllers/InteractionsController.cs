5af3ea5417e529e01c3901fcbd9aa90fb586975d
KacperWro <adamczyk911119@gmail.com>
2024-01-22 17:07:42 +0000

    Begin work on logic to compare two codes

---
+++ Controllers/InteractionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PalletSyncApi.Services;
+using Newtonsoft.Json.Linq;
+using PalletSyncApi.Classes;
+
+namespace PalletSyncApi.Controllers
+{
+    [ApiController]
+    [Route("Interactions")]
+    public class InteractionsController
+    {
+        private readonly IForkliftService _forkliftService;
+        private readonly IPalletService _palletService;
+        private readonly IShelfService _shelfService;
+
+        [HttpPost]
+        public async Task<IActionResult> CompareTwoCodes([FromBody] JObject data)
+        {
+            try
+            {
+                Pallet pallet = data["palletData"].ToObject<Pallet>();
+                Shelf shelf = data["shelfData"].ToObject<Shelf>();
+
+                //TODO: Add state "misplaced" for pallets that are placed on the incorrect shelf
+            }
+            catch
+            {
+                Console.WriteLine("Some bullshit happened");
+            }
+
+            return null;
+        }
+    }
+}
