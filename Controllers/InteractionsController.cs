6ae84b1c5eee080430160fe58a94873a76d0392d
Nfedans <nfedans@gmail.com>
2024-01-31 13:21:45 +0000

    Release 1 sprint 9: initial endpoint created for the forklift to be track when a pallet is lifted from the floor or placed onto the floor, untested

--- Controllers/InteractionsController.cs
+++ Controllers/InteractionsController.cs
@@ -3,13 +3,15 @@ using PalletSyncApi.Services;
 using Newtonsoft.Json.Linq;
 using PalletSyncApi.Classes;
 using PalletSyncApi.Context;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.EntityFrameworkCore;
 
 namespace PalletSyncApi.Controllers
 {
 
     [ApiController]
     [Route("Interactions")]
-    public class InteractionsController: ControllerBase
+    public class InteractionsController : ControllerBase
     {
         private readonly IPalletService _palletService = new PalletService();
         private readonly IShelfService _shelfService = new ShelfService();
@@ -18,7 +20,7 @@ namespace PalletSyncApi.Controllers
 
         PalletSyncDbContext context = new PalletSyncDbContext();
 
-        [HttpPost]
+        [HttpPost("TwoCodes")]
         public async Task<IActionResult> CompareTwoCodes([FromBody] CompareTwoCodesJson data)
         {
             try
@@ -27,7 +29,7 @@ namespace PalletSyncApi.Controllers
                 bool shelfFound = context.Shelves.Where(s => s.Id == data.Shelf.Id).FirstOrDefault() != null;
                 bool forkliftFound = context.Forklifts.Where(f => f.Id == data.ForkliftId).FirstOrDefault() != null;
 
-                if(!palletFound || !shelfFound)
+                if (!palletFound || !shelfFound)
                 {
                     return NotFound($"The given pallet and/or shelf code does not appear in the database. " +
                         $"Please rescan the codes or enter them manually. " +
@@ -47,14 +49,14 @@ namespace PalletSyncApi.Controllers
                 var palletCode = data.Pallet.Id.Substring(data.Pallet.Id.IndexOf("-") + 1);
                 var shelfCode = data.Shelf.Id.Substring(data.Shelf.Id.IndexOf("-") + 1);
 
-                if(palletCode == shelfCode)
+                if (palletCode == shelfCode)
                 {
                     return Ok($"Pallet code and shelf code are matching. All is good");
                 }
                 else
                 {
                     var correctShelf = context.Shelves.Where(s => s.Id.Contains(palletCode)).FirstOrDefault();
-                    if(correctShelf != null)
+                    if (correctShelf != null)
                         return ValidationProblem($"Pallet code and shelf codes {data.Pallet.Id} and {data.Shelf.Id} do not match. " +
                             $"This pallet code belongs in shelf {correctShelf.Id} which is located in {correctShelf.Location}");
                     else
@@ -65,9 +67,76 @@ namespace PalletSyncApi.Controllers
                 //TODO: Add state "misplaced" for pallets that are placed on the incorrect shelf
                 //TODO: Accuracy of code scan
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return BadRequest($"Something unexpected went wrong: {ex.Message}");
+            }
+        }
+
+        [HttpPost("OneCode")]
+        public async Task<IActionResult> DealWithDeployedPallet([FromBody] CompareTwoCodesJson data)
+        {
+            try
+            {
+                bool palletFound = context.Pallets.Where(p => p.Id == data.Pallet.Id).FirstOrDefault() != null;
+                bool forkliftFound = context.Forklifts.Where(f => f.Id == data.ForkliftId).FirstOrDefault() != null;
+
+                if (!palletFound)
+                {
+                    return NotFound($"The given pallet code does not appear in the database. " +
+                        $"Please rescan the codes or enter them manually. " +
+                        $"If the codes appear correct and you are still encountering an error, please contact an administrator");
+                }
+                if (!forkliftFound)
+                {
+                    return NotFound($"Forklift id {data.ForkliftId} could not be found in the database. Please contact an administrator");
+                }
+
+                bool pickingPalletOffFloor = await ValidateStateChangeForPalletCodeOnly(data.Pallet);
+
+                await _palletService.UpdatePalletAsync(data.Pallet, true);
+
+                if (pickingPalletOffFloor)
+                {
+                    // Dont think this step is necessary when putting the pallet onto the floor                                                          
+                    await _forkliftService.UpdateForkliftAfterScan(data.ForkliftId, data.Pallet.Id, data.UserId);
+                }
+                await _palletTrackingLogService.AddPalletTrackingLogAsync(data);
+
+                if (pickingPalletOffFloor)
+                    return Ok($"Pallet " + data.Pallet.Id + " picked up from location: " + data.Pallet.Location);
+                else
+                    return Ok($"Pallet " + data.Pallet.Id + " left at from location: " + data.Pallet.Location);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Something unexpected went wrong: {ex.Message}");
+            }
+        }
+
+        private async Task<bool> ValidateStateChangeForPalletCodeOnly(Pallet newPallet)
+        {
+            Pallet oldPallet = await context.Pallets.FirstOrDefaultAsync(p => p.Id == newPallet.Id);
+
+            if (oldPallet.State == Enums.PalletState.Floor && !string.IsNullOrEmpty(oldPallet.Location)
+                && !string.IsNullOrWhiteSpace(oldPallet.Location) && newPallet.State == Enums.PalletState.Fork
+                && string.IsNullOrEmpty(newPallet.Location))
+            {
+                // This checks that our pallet starts off in a valid expected state (sitting on the floor somewhere)
+                // And when the forklift picks it up, that the new state of the pallet is also valid and what we expect
+                return true;
+            }
+            else if (oldPallet.State == Enums.PalletState.Fork && string.IsNullOrEmpty(oldPallet.Location)
+                && string.IsNullOrWhiteSpace(oldPallet.Location) && newPallet.State == Enums.PalletState.Floor
+                && !string.IsNullOrEmpty(newPallet.Location) && !string.IsNullOrWhiteSpace(newPallet.Location))
+            {
+                // This checks that our pallet starts off in a valid expected state (sitting on forklift)
+                // And when the forklift tries to put it on the floor, that the new state of the pallet is also valid and what we expect
+                return false;
+            }
+            else
+            {
+                throw new InvalidOperationException("the database is inconsistent or wrong instruction was sent by the forklift operator, please contact the admin!");
             }
         }
     }
