718a69a7c3a99eab06049ae3b914d55dee7fe15a
KacperWro <adamczyk911119@gmail.com>
2024-02-02 10:34:06 +0000

    Temporarily comment out regex

--- Classes/Pallet.cs
+++ Classes/Pallet.cs
@@ -6,7 +6,7 @@ namespace PalletSyncApi.Classes
     public class Pallet
     {
         [Required]
-        [Regex(@"^P-[0-9]+$", ErrorMessage = "ID must be in the P-1234 format")]
+        //[Regex(@"^P-[0-9]+$", ErrorMessage = "ID must be in the P-1234 format")]
         [StringLength(450, ErrorMessage = "The id must be 450 characters or less.")]
         public string Id { get; set; } //Pallet code
         public PalletState State { get; set; }
--- Classes/Shelf.cs
+++ Classes/Shelf.cs
@@ -5,7 +5,7 @@ namespace PalletSyncApi.Classes
     public class Shelf
     {
         [Required]
-        [Regex(@"^S-[0-9]+$", ErrorMessage = "ID must be in the S-1234 format")]
+        //[Regex(@"^S-[0-9]+$", ErrorMessage = "ID must be in the S-1234 format")]
         [StringLength(450, ErrorMessage = "The id must be 450 characters or less.")]
         public string Id { get; set; } //Shelf code
         public string? PalletId { get; set; }
